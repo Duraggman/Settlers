852f46bee8ac12de2b92401281f8f01f45db85fd
Tyrell <tyrellgw99@gmail.com>
2023-03-22 04:59:45 +0000

    v0.7: Added the Ai selection section. Finished the startup sequence.

--- Mechanics/Start.py
+++ Mechanics/Start.py
@@ -21,29 +21,68 @@ def game_type():  # game type selection.
             print(" ")
 
 
-def timer():
+def timer():  # The player selects how long they want the quick game to last.
     """
     Allows the player to decide how long the want the quick game to last in minutes.
     :return: how long the game will last in minutes.
     """
     picked = False
     while not picked:
-        time = input("How long do you want the game to last: ")
+        time = input("How long do you want the game to last. (In minutes): ")
         if time.isnumeric():
-            return time
+            return time * 60
         else:
-            print("only numbers will allowed as an input.")
+            print("only numbers are allowed as an input.")
             print(" ")
 
 
 def num_of_players():  # selecting the number of players that will be in the game
-    pass
+    """
+    Allows the player to choose how many player roles the want in the settlers game. Also allows the player to decide if
+    they want an AI player in the game.
+
+    Ais: The amount of Ais that will be in the game
+    players: the amount of players in the game
+    picked: assigned false until the number of players and AI amount has been assigned
+    ai_chosen: loops until AI amount is chosen.
+    ai_picker: checks if AI are wanted in the settlers game.
+
+    :return: Ais and players
+    """
+    Ais = 0  # number of Ais in the game 
+    players = 0  # number of players
+    picked = False  # Controls if the 
+    while not picked:
+        try:
+            players = int(input("How many players are there?"))
+            if players > 4 or players < 1:
+                print("only numbers between 1-4 are allowed.")
+            else:
+                # Ai selection
+                ai_chosen = False
+                while not ai_chosen:
+                    ai_picker = input("Do you want ai players?(Y/N): ").upper()
+                    if ai_picker == "Y":
+                        ai_chosen = True
+                        players = players - 1
+                        Ais = 1
+                    elif ai_picker == "N":
+                        ai_chosen = True
+                    else:
+                        print(" ")
+                        print("Only Y/N are accepted.")
+        except ValueError:
+            print("Only numbers are allowed.")
+        if Ais == 0:
+            return players
+        if Ais == 1:
+            return Ais, players
 
 
 class Start:
-    if game_type():
+    gameType = game_type()
+    if gameType:
         pass
     else:
-        timer()
-    num_of_players()
-    pass
+        amount_of_time = timer()
+    num_players = num_of_players()
--- Mechanics/test_Start.py
+++ Mechanics/test_Start.py
@@ -1,17 +1,36 @@
-def test_start():
-    test_return = bool
-    full_or_quick = input("Do you wanna play a full game or a quick game").lower()
-    if full_or_quick == "full":
-        test_return = True
-    elif full_or_quick == "quick":
-        test_return = False
-    else:
-        print("only 'full' or  'quick' will be accepted.")
-    if not test_return or test_return:
-        assert True
-    else:
-        assert False
+import unittest
+from unittest import TestCase
 
 
-def test_timer():
-    assert False
+class TestStart(unittest.TestCase):
+    def test_game_type(self):  # test for game type selection
+        picked = False
+        while not picked:
+            test_return = bool
+            full_or_quick = input("Do you wanna play a full game or a quick game").lower()
+            if full_or_quick == "full":
+                test_return = True
+            elif full_or_quick == "quick":
+                test_return = False
+            else:
+                print(" ")
+                print("only 'full' or  'quick' will be accepted.")
+            if not test_return or test_return:
+                assert True
+            else:
+                assert False
+
+    def test_timer(self):
+        time = input("How long do you want the game to last: ")
+        if time.isnumeric():
+            print("Passed")
+            assert True
+        elif not time.isnumeric():
+            print("Failed")
+            assert False
+        else:
+            print("Passed")
+            assert True
+
+    def test_num_of_players(self):
+        self.fail()
